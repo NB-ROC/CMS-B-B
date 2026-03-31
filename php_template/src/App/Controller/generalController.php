f388762f008eb3cce35366fbcd7d92efccde832e
TooHighDude420 <nataro112@gmail.com>
2026-03-31 13:28:51 +0200

    added a general controller

--- php_template/src/App/Controller/generalController.php
+++ php_template/src/App/Controller/generalController.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace App\Controller;
+
+class GeneralController
+{
+    public static function linkToAction(string $action)
+    {
+        return "php/$action.php";
+    }
+
+    public static function linkTo(string $location)
+    {
+        return "index.php?page=$location";
+    }
+
+
+}
+
