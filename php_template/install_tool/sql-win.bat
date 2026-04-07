e43f17e1b83538cb285a5afa00a02ca016165345
TooHighDude420 <nataro112@gmail.com>
2026-04-07 19:09:02 +0200

    added sql install tool

---
+++ php_template/install_tool/sql-win.bat
@@ -0,0 +1,5 @@
+@echo off
+python3 -m venv venv
+.\venv\Scripts\activate
+pip install -r requirements.txt
+python3 .\initDB.py
--- php_template/start_project.bat
+++ php_template/start_project.bat
@@ -1,4 +1,7 @@
 @echo off
 npm install
+npm audit fix
 docker compose up -d
+cd .\install_tool\
+sql-win.bat
 pause
