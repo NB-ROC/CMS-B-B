364da74529d26c13351a0eb1a414a41d5ab47088
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-05-22 16:01:06 +0200

    small commit
    nothing importand

--- php_template/src/App/View/reserveringen.inc.php
+++ php_template/src/App/View/reserveringen.inc.php
@@ -30,7 +30,7 @@
 
             <!-- Description -->
             <div class="h-[180px] w-[125%] scale-x-[-1] overflow-y-scroll pr-[10px] box-border">
-                <p class="scale-x-[-1]">
+                <p class="scale-x-[-1] text-black">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
