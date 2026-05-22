9d90a6fbaf36babdbefda160eb3f952b11b0bf2c
sennaTia <1214998@student.roc-nijmegen.nl>
2026-05-22 14:50:21 +0200

    tried to get review back partly worked

--- php_template/src/index.php
+++ php_template/src/index.php
@@ -58,8 +58,10 @@ $request = Route::get_uri();
         Route::render($request);
         ?>
     </main>
-    <footer>
-
+        <footer>
+        <?php
+            Route::render_component("reviews", []);
+        ?>
     </footer>
 </body>
 
