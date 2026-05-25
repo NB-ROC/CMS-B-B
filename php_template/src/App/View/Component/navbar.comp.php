b3895de7976a36ed7e4195640c5759923cd010ba
simplyana08 <ana.t08@yahoo.com>
2026-05-25 02:12:54 +0200

    header gefixt

--- php_template/src/App/View/Component/navbar.comp.php
+++ php_template/src/App/View/Component/navbar.comp.php
@@ -2,14 +2,18 @@
 use App\Statics\Route;
 ?>
 
-<div class="logo">
-    <img src="/images/imagegroot.png" alt="Calm Corner hotel">
-</div>
+<header class="header">
 
-<nav>
-    <?php 
-        foreach($pages as $page){
-            echo("<a href=". Route::linkTo($page)."> $page </a>");
-        }
-    ?>
-</nav>
+    <div class="logo">
+        <img src="/images/imagegroot.png" alt="Calm Corner hotel">
+    </div>
+
+    <nav>
+        <?php 
+            foreach($pages as $page){
+                echo("<a href='". Route::linkTo($page)."'>$page</a>");
+            }
+        ?>
+    </nav>
+
+</header>
