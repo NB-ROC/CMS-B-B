d9f908590cee9f0374b247cf37cbcd705394f4d0
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-05-29 10:55:34 +0200

    Translated Css to tailwind
    -Translated the page to tailwind pretty sure everything will look correct now

--- php_template/src/App/View/uploadFile.inc.php
+++ php_template/src/App/View/uploadFile.inc.php
@@ -1,14 +1,14 @@
 <!-- Main -->
 <div class="h-screen px-[100px] py-5 box-border">
   <!-- Form section -->
-  <form action="php/upload.php" method="post" class="flex flex-col gap-[10px] bg-white shadow-md p-[15px] rounded-[15px]">
+  <form action="php/upload.php" method="post" class="w-[95%] flex flex-col gap-[10px] bg-white shadow-md p-[15px] rounded-[15px]">
     <h2>Upload a file</h2>
     <p class="text-black">Testing page to upload images for the website.</p>
 
     <!-- Upload file section -->
-    <input type="file" name="fileToUpload" id="fileToUpload" class="rounded-[15px] border border-black px-[15px] py-[5px] w-fit">
+    <input type="file" name="fileToUpload" id="fileToUpload" class="text-black rounded-[15px] border border-black px-[15px] py-[5px] w-fit">
     
     <!-- Submit file section -->
-    <input type="submit" value="Upload Image" name="submit" class="booking-button">
+    <input type="submit" value="Upload file" name="submit" class="booking-button">
   </form>
 </div>
