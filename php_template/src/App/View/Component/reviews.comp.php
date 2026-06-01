ddfc926944005ee204bce720525d038053d19883
sennaTia <1214998@student.roc-nijmegen.nl>
2026-06-01 09:23:11 +0200

    Made php work

--- php_template/src/App/View/Component/reviews.comp.php
+++ php_template/src/App/View/Component/reviews.comp.php
@@ -1,5 +1,26 @@
 <?php
+use App\Statics\DatabaseSingleton;
 use App\Statics\Route;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    isset(DatabaseSingleton::$conn) ? : DatabaseSingleton::makeCon();
+
+    $name = $_POST["name"];
+    $mess = $_POST["message"];
+    $rating = $_POST["rating"];
+
+    $connHolder = DatabaseSingleton::$conn;
+
+    $sql = "INSERT INTO reviews (naam, bericht, rating) VALUES (:naam, :mess, :rating)";
+
+    $stmt = $connHolder->prepare($sql);
+
+    $stmt->execute([
+        ":naam" => $name,
+        ":mess" => $mess,
+        ":rating" => $rating
+    ]);
+}
 ?>
 
 <section class="review-section">
