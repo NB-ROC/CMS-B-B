f34e9c484a4283a63aaa8c8c12367a7098555371
TooHighDude420 <nataro112@gmail.com>
2026-06-04 16:54:47 +0200

    made roles table and added fk to users

--- php_template/src/Maker_Out/SQL/DataBaseLayoutUsers.sql
+++ php_template/src/Maker_Out/SQL/DataBaseLayoutUsers.sql
@@ -1,3 +1,8 @@
-UsersID INT PRIMARY KEY auto_increment NOT NULL,
-username VARCHAR(255) NOT NULL,
+-- @phase one
+UsersID INT PRIMARY KEY auto_increment NOT NULL
+username VARCHAR(255) NOT NULL
 password VARCHAR(255) NOT NULL
+role INT NOT NULL
+
+-- @phase two
+ADD CONSTRAINT Users_Roles_FK FOREIGN KEY (`role`) REFERENCES bnb_database.Roles(RolesID)
