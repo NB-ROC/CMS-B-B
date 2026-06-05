864d0f45f8046bf8d4741ed5e57f3bc6940381b6
JSpanjaard07 <joeyamgelo123@outlook.com>
2026-06-05 16:04:51 +0200

    More home tailwind

--- php_template/src/App/View/home.inc.php
+++ php_template/src/App/View/home.inc.php
@@ -9,42 +9,42 @@
     </div>
 </div>
 <div class="w-full flex flex-col items-center gap-[50px] p-[30px] pb-[100px]">
-    <h2 class="head-section-header-text">Faciliteiten</h2>
-    <div class="faciliteiten-info-container-row">
-        <div class="faciliteiten-info-container-column">
-            <div class="faciliteiten-info-box-container">
+    <h2 class="text-[60px]">Faciliteiten</h2>
+    <div class="w-[95%] flex flex-row justify-between">
+        <div class="flex flex-col gap-[30px]">
+            <div class="w-[400px] bg-white rounded-[20px] outline outline-1 outline-[#d2b8b8] flex flex-col gap-[20px] p-[20px] pb-[50px]">
                 <img class="faciliteiten-icon" src="images/wifi.png" alt="wifi">
                 <h3>Gratis wifi</h3>
                 <p>Snel internet overal</p>
             </div>
 
-            <div class="faciliteiten-info-box-container">
+            <div class="w-[400px] bg-white rounded-[20px] outline outline-1 outline-[#d2b8b8] flex flex-col gap-[20px] p-[20px] pb-[50px]">
                 <img class="faciliteiten-icon" src="images/P.png" alt="wifi">
                 <h3>Gratis parkeerplaats</h3>
                 <p>Privé parkeerplaats</p>
             </div>
 
-            <div class="faciliteiten-info-box-container">
+            <div class="w-[400px] bg-white rounded-[20px] outline outline-1 outline-[#d2b8b8] flex flex-col gap-[20px] p-[20px] pb-[50px]">
                 <img class="faciliteiten-icon" src="images/location.png" alt="wifi">
                 <h3>Centrale locatie</h3>
                 <p>Nabij attracties</p>
             </div>
         </div>
 
-        <div class="faciliteiten-info-container-column">
-            <div class="faciliteiten-info-box-container">
+        <div class="flex flex-col gap-[30px]">
+            <div class="w-[400px] bg-white rounded-[20px] outline outline-1 outline-[#d2b8b8] flex flex-col gap-[20px] p-[20px] pb-[50px]">
                 <img class="faciliteiten-icon" src="images/cofee.png" alt="wifi">
                 <h3>Ontbijt inbegrepen</h3>
                 <p>Vers en lokaal</p>
             </div>
 
-            <div class="faciliteiten-info-box-container">
+            <div class="w-[400px] bg-white rounded-[20px] outline outline-1 outline-[#d2b8b8] flex flex-col gap-[20px] p-[20px] pb-[50px]">
                 <img class="faciliteiten-icon" src="images/shield.png" alt="wifi">
                 <h3>24/7 Veiligheid</h3>
                 <p>Beveiligde omgeving</p>
             </div>
 
-            <div class="faciliteiten-info-box-container">
+            <div class="w-[400px] bg-white rounded-[20px] outline outline-1 outline-[#d2b8b8] flex flex-col gap-[20px] p-[20px] pb-[50px]">
                 <img class="faciliteiten-icon" src="images/star.png" alt="wifi">
                 <h3>5-Sterren Service</h3>
                 <p>Uitstekende reviews</p>
