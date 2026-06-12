1db65a7370b9cab63bf090e3101fa7a6df16bc16
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-06-12 15:49:59 +0200

    Succes page style commit

--- php_template/src/App/View/uploadResult.inc.php
+++ php_template/src/App/View/uploadResult.inc.php
@@ -1,19 +1,30 @@
-<h2 class="testWow">
+<div class="mx-auto my-5 flex w-[85%] flex-col rounded-[10px] bg-white px-[35px] pt-[15px] pb-[20px] shadow-md">
+    <h2>
+        <?php
+            if($succes){
+                echo "Succes!";
+            }else{
+                echo "Oh no!";
+            }
+        ?>
+    </h2>
+    <p class="text-black">
+        <?php
+            echo $message;
+        ?>
+    </p>
     <?php
-        echo $message;
+        if($succes){
     ?>
-</h2>
-
-<?php
-    if($succes){
-?>
-        <img 
-            <?php
+        <div class="pt-5">
+            <img 
+                class="w-[30%] shadow-md"
+                <?php
                 echo  'src="./images/testFile/'.$fileName.'"';
-            ?>
-        >
-<?php
-    }else{
-        echo '<p class="testWow">Nah</p>';
-    }
-?>
+                ?>
+            >
+        </div>
+    <?php
+        }
+    ?>
+</div>
