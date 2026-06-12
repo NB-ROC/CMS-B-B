82225ff5c295c606bb3676304bc62014758ca06d
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-06-12 15:17:48 +0200

    Upload file result page.
    -When you try to upload a file youll get a result page.

--- php_template/src/App/Controller/UploadImageController.php
+++ php_template/src/App/Controller/UploadImageController.php
@@ -16,26 +16,26 @@ class UploadImageController
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
             if ($check !== false) {
             } else {
-                return Route::render("/uploadResult", ["message" => "File is not an image."]);
+                return Route::render("/uploadResult", ["message" => "File is not an image.", "succes" => false]);
             }
         }
         // Check if file already exists
         if (file_exists($target_file)) {
-            return Route::render("/uploadResult", ["message" => "Sorry, file has the same name as an existing file."]);
+            return Route::render("/uploadResult", ["message" => "Sorry, file has the same name as an existing file.", "succes" => false]);
         }
         // Check file size
         if ($_FILES["fileToUpload"]["size"] > 5000000) {
-            return Route::render("/uploadResult", ["message" => "Sorry, your file is too large."]);
+            return Route::render("/uploadResult", ["message" => "Sorry, your file is too large.", "succes" => false]);
         }
         // Allow certain file formats
         if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-            return Route::render("/uploadResult", ["message" => "Sorry, only JPG, JPEG and PNG files are allowed."]);
+            return Route::render("/uploadResult", ["message" => "Sorry, only JPG, JPEG and PNG files are allowed.", "succes" => false]);
         }
 
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            return Route::render("/uploadResult", ["message" => "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded."]);
+            return Route::render("/uploadResult", ["message" => "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.", "succes" => true, "fileName" => htmlspecialchars(basename($_FILES["fileToUpload"]["name"]))]);
         } else {
-            return Route::render("/uploadResult", ["message" => "Sorry, there was an error uploading your file."]);
+            return Route::render("/uploadResult", ["message" => "Sorry, there was an error uploading your file.", "succes" => false]);
         }
     }
 }
--- php_template/src/App/View/uploadResult.inc.php
+++ php_template/src/App/View/uploadResult.inc.php
@@ -3,3 +3,20 @@
         echo $message;
     ?>
 </h2>
+<h2>
+    <?php
+        if($succes){
+    ?>
+            <img 
+            <?php
+                echo  'src="./images/testFile/'.$fileName.'"';
+            ?>
+            alt="Trulli" 
+            width="500" 
+            height="333">
+    <?php
+        }else{
+            echo "Nah";
+        }
+    ?>
+</h2>
